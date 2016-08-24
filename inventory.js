585c930e34cc6386ea5bd8c4699e274f7fc526ca
Cody Ogden <cody@codyogden.com>
2016-08-24 08:46:15 -0500

    stuff

--- inventory.js
+++ inventory.js
@@ -13,19 +13,39 @@ var addItem = function( color, name, size ){
   	size:  size
   });
   // push into array
-  inventory.push(item);
+  if ( inventory.push(item) ) return true;
   // show updated inventory
   console.log(inventory);
 } // end addItem
 
-var findItems = function( size, color ) {
+var addItemFromUser = function ( ) {
+  console.log( "in addItemFromUser" );
+  theName = document.getElementById( 'nameInput' );
+  theColor = document.getElementById( 'colorInput' );
+  theSize = document.getElementById( 'sizeInput' );
+  messageBox = document.getElementById( 'productAddedMessage' );
+  if( theName.value != "" && theColor.value != "--" && theSize.value != "--" ) {
+    addItem( theColor.value, theName.value, theSize.value );
+    document.getElementById( 'nameInput' ).value = "";
+    document.getElementById( 'sizeInput' ).value = "--";
+    document.getElementById( 'colorInput' ).value = "--";
+    messageBox.innerHTML = "Product Added!";
+    setTimeout(function(){ messageBox.innerHTML = ""; }, 2000);
+  } else {
+    alert("All fields are required!!");
+    return false;
+  }
+}
+
+var findItems = function( size, color, name ) {
 	// Create items array
 	var items = [];
 	// Search through all the items in our inventory
 	for ( var i = 0; i < inventory.length; i++ ) {
 		// If the item is a match, add it to the items array
-		if ( inventory[i].size === size && inventory[i].color === color ) {
-			items.push( inventory[i].name );
+    var productName = inventory[i].name + "";
+		if ( ( inventory[i].size === size && inventory[i].color === color ) || productName.toLowerCase() === name.toLowerCase() ) {
+			items.push( inventory[i] );
 		}
 	}
 	return items;
@@ -38,14 +58,17 @@ var searchInventory = function(){
   selectedSize = document.getElementById( 'sizeIn' ).value;
   // get color from user
   selectedColor = document.getElementById( 'colorIn' ).value;
+  // get name of product from user search
+  selectedName = document.getElementById( 'nameIn' ).value;
   console.log( 'looking for something ' + selectedColor + ' and ' + selectedSize );
   // check if an item fits this description
-  var items = findItems(selectedSize, selectedColor);
+  var items = findItems( selectedSize, selectedColor, selectedName );
   // If we get items, log out each item's name.
   if( items.length > 0 ) {
     document.getElementById("items").innerHTML = "";
+
   	for( var j = 0; j < items.length; j++ ) {
-      document.getElementById("items").innerHTML += "<li>" + items[j] + "</li>";
+      document.getElementById("items").innerHTML += "<li>We found a " + items[j].name + " that is " + items[j].size + " and " + items[j].color + "</li>";
   		console.log( "Match:", items[j] );
   	} 
   } else {
@@ -56,6 +79,7 @@ var searchInventory = function(){
 
 ///////// ADD ITEMS /////////
 addItem( 'blue', 'Smurf', 'Small');
+addItem( 'blue', 'blueberry', 'Small');
 addItem( 'mermaid treasure', 'Prime Academy', 'Large');
 addItem( 'bus seat green', 'Baby Banner', 'Medium');
 addItem( 'purple', 'Eggplant', 'Small');
